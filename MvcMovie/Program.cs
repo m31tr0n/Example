f4d49d38570e35034e98b3a2ee5c753bd95c3ab3
m31tr0n <dashulay173@gmail.com>
2025-05-07 15:36:42 +0300

    часть 5

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PagesMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PagesMovieContext") ?? throw new InvalidOperationException("Connection string 'PagesMovieContext' not found.")));
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<PagesMovieContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
